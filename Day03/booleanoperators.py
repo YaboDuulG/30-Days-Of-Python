4d4264014c5985197425bcfab12c53aba1f1d10f
YaboDuulG <134439724+YaboDuulG@users.noreply.github.com>
2024-06-30 02:12:26 -0600

    Create booleanoperators.py
    Started on Day 3

---
+++ Day03/booleanoperators.py
@@ -0,0 +1,55 @@
+#Exercises - Day 3
+age = 22
+height = 5.916666667
+complex = 4+88j
+
+# Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h)
+base = input("Enter base: ")
+height = input("Enter height: ")
+area = 0.5*base*height
+
+# Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c)
+a = input("Enter side a: ")
+b = input("Enter side b: ")
+c = input("Enter side c: ")
+perimeter = a + b + c
+
+# Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+rec_length = input("Enter length of rectangle: ")
+rec_width = input("Enter width of rectangle: ") 
+rec_area = rec_length*rec_width
+rec_perimeter = 2 x (rec_length + rec_width)
+
+# Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+radius = input("Enter length of rectangle: ")
+circle_area = 3.14*radius*radius
+circumference = 2*3.14*radius
+
+
+
+Calculate the slope, x-intercept and y-intercept of y = 2x -2
+Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+Compare the slopes in tasks 8 and 9.
+Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+Use and operator to check if 'on' is found in both 'python' and 'dragon'
+I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+There is no 'on' in both dragon and python
+Find the length of the text python and convert the value to float and convert it to string
+Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+Check if type of '10' is equal to type of 10
+Check if int('9.8') is equal to 10
+Writ a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+Enter hours: 40
+Enter rate per hour: 28
+Your weekly earning is 1120
+Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+Enter number of years you have lived: 100
+You have lived for 3153600000 seconds.
+Write a Python script that displays the following table
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
